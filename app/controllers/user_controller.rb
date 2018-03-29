882842de63f158e18668ecef3c7cd4861a453c7d
thevirtualman <peadarh10@gmail.com>
2018-03-29 13:42:17 +0000

    Login / logout added

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+  
+  def login
+    session[:login] = 1
+    flash[:notice] = "You are logged in"
+    redirect_to :controller => :items
+  end
+  
+  def logout
+    session[:login] = nil
+    flash[:notice] = "You logged out"
+    redirect_to :controller => :items
+  end
+  
+  
+end
